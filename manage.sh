0f5930b8ece133283655ac4f4cdc669576d17d7a
jtrrll <jacksonterrill3@gmail.com>
2023-03-20 15:24:25 -0400

    created main dotfile management script

---
+++ manage.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+shopt -s nullglob   # expands unmatched wildcards to null
+
+function usage() {
+  cat <<USAGE
+Usage: $0 [OPTION]
+Options:
+  -s, --sync:     Sync system configs with dotfiles repository
+  -r, --remove:   Remove links to dotfiles
+  -h, --help:     Display usage and available options
+USAGE
+}
+
+if [ $# -ne 1 ]; then
+  usage
+  exit 1
+fi
+
+while [ "$1" != "" ]; do
+  case $1 in
+  -s | --sync)
+    for dir in */; do
+      (cd $dir; bash manage*.sh -s)
+    done
+  ;;
+  -r | --remove)
+    for dir in */; do
+      (cd $dir; bash manage*.sh -r)
+    done
+  ;;
+  -h | --help)
+    usage
+    ;;
+  *)
+    usage
+    ;;
+  esac
+  shift
+done

bf00b65010aa67f66634381bb039217a5bb7e058
Jackson Terrill <77407057+jacksonterrill@users.noreply.github.com>
2023-03-24 14:57:24 -0400

    shellchecked and made POSIX compliant

--- manage.sh
+++ manage.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-shopt -s nullglob   # expands unmatched wildcards to null
-
-function usage() {
+usage() {
   cat <<USAGE
-Usage: $0 [OPTION]
+Usage: $0 OPTION
 Options:
   -s, --sync:     Sync system configs with dotfiles repository
   -r, --remove:   Remove links to dotfiles
@@ -20,12 +18,14 @@ fi
 case $1 in
   -s | --sync)
     for dir in */; do
-      (cd $dir; bash manage*.sh -s)
+      [ -e "$dir" ] || break
+      (cd "$dir" || exit; ./manage.sh -s)
     done
   ;;
   -r | --remove)
     for dir in */; do
-      (cd $dir; bash manage*.sh -r)
+      [ -e "$dir" ] || break
+      (cd "$dir" || exit; ./manage.sh -r)
     done
   ;;
   -h | --help)

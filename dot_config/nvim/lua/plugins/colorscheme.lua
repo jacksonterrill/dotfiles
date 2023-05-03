3b18fc45e3901faf71d25536d7f0903b4223de9d
jtrrll <jacksonterrill3@gmail.com>
2023-05-03 14:28:20 -0400

    added neovim config (LazyVim)

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,32 @@
+return {
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup({
+        flavour = "macchiato",
+        integrations = {
+          lsp_saga = true,
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          telescope = true,
+          hop = true,
+          fidget = true,
+          treesitter = true,
+          mason = true,
+          illuminate = true,
+          lsp_trouble = true,
+          notify = true,
+        },
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}

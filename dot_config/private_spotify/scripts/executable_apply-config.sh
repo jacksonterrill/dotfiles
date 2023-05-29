78f84d35864dea238a5ddebc2ea45692c8d0d8e6
jtrrll <jacksonterrill3@gmail.com>
2023-05-28 23:22:24 -0400

    added spicetify installer

---
+++ dot_config/private_spotify/scripts/executable_apply-config.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+error() {
+  printf "Error: %s\n" "$1" 1>&2
+  exit 1
+}
+
+if [ "$#" -ne 0 ]; then
+  error "Invalid arguments"
+fi
+
+if ! command -v spotify > /dev/null; then
+  error "Spotify not installed"
+fi
+
+if ! sh <(curl -sSL https://raw.githubusercontent.com/SpotX-CLI/SpotX-Linux/main/install.sh); then
+  error "Could not install SpotX adblocker"
+fi
+
+if ! curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.sh | sh; then
+  error "Could not install Spicetify"
+fi
+
+spicetify backup apply
+
+if ! curl -fsSL https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.sh | sh; then
+  error "Could not install Spicetify Marketplace"
+fi
+
+printf "Successfully applied custom Spotify config\n"
+exit 0
